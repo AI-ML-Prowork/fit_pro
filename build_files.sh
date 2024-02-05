bb7dbfe7bc6e4e91a6dc058c042a696774636f04
CFC-Tech-AI <pythoncfc@gmail.com>
2024-02-05 10:40:27 +0530

    changes_sh_file

--- build_files.sh
+++ build_files.sh
@@ -4,8 +4,6 @@ python3.9 -m pip install -r requirements.txt
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
-python3.9 manage.py createsuperuser --noinput \
-    --username admin \
-    --password admin@123
+
     
 python3.9 manage.py collectstatic --noinput --clear
